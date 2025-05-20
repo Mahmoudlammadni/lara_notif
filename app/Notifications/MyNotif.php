79314a4344d8cbf8261c37150c6cc0a1a31e442e
Mahmoud <lamm.mah2002@gmail.com>
2025-05-21 00:19:49 +0200

    add notification via databases

---
+++ app/Notifications/MyNotif.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class MyNotif extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+{
+    return ['database'];
+}
+
+public function toDatabase(object $notifiable): array
+{
+    return [
+        'message' => 'Your order has been shipped!',
+        'url' => url('/orders/123')
+    ];
+}
+
+
+
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
